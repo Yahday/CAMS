f30b060945f672e0c59fe10faeedd8e1371a1bb6
Yahday A <64969363+Yahday@users.noreply.github.com>
2020-05-19 22:14:10 -0500

    Integracion de Frontend con Backend para Login
    Integracion de Frontend de JoseLuis con el Backend

--- BackEnd/routes/login.js
+++ BackEnd/routes/login.js
@@ -10,33 +10,25 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => { //async para db
-    //falta busqueda en la bd para comprobar usuario y contraseña
-    /*
-    const { email, password } = req.body;
-    const user = await User.findOne({email: email});
-    if (!user) return res.status(401).send("User do not exist");
-    if (user.password !== password) return res.status(401).send("Wrong Password");
-    */
-    //cambiar por datos de bd
-    //Texto para test= INICIO
-    if(req.body.usuario === "Yahday" && req.body.contrasena === "Awad") {
-        const payload = {
-         check:  true,
-         userId: req.body._id //user._id
-        };
-    //Texto para test= FIN    
-        const token = jwt.sign(payload, process.env.SEED, {
-            expiresIn: 1440
-        });
 
-        res.json({
-         mensaje: 'Autenticación correcta',
-         token: token,
-         _id: payload.userId
-        });
-    } else {
-      res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }    
+    /*falta busqueda en la bd para comprobar usuario y contraseña
+    const { email, password } = req.body;
+    const user = await User.findOne({email: email});*/
+    if (req.body.email !== "Yahday") return res.status(401).send("User do not exist");//(!user)
+    if (req.body.password !== "Awad") return res.status(401).send("Wrong Password");//(user.password !== password)
+    const payload = {
+        check:  true,
+        userId: req.body._id //user._id
+       };
+    const token = jwt.sign(payload, process.env.SEED, {
+        expiresIn: 1440
+    });
+
+    res.json({
+     mensaje: 'Autenticación correcta',
+     token: token,
+     _id: payload.userId
+    });
 }) 
 
 module.exports = app;
