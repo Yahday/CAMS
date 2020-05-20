e2e005e2af6719d6626871e35a73a24d90ec00c4
Vanessa-14 <55520149+Vanessa-14@users.noreply.github.com>
2020-05-20 18:10:46 -0500

    conflic

--- BackEnd/routes/login.js
+++ BackEnd/routes/login.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/users');
-//MODEL OF BD
-const app = express();
+// const express = require('express');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/users');
+// //MODEL OF BD
+// const app = express();
 
-app.get('/login', (req, res) => {
-    res.send('<h1>LOGIN</h1>');
-});
+// app.get('/login', (req, res) => {
+//     res.send('<h1>LOGIN</h1>');
+// });
 
-//ruta
-app.post('/login', async(req, res) => {
+// //ruta
+// app.post('/login', async(req, res) => {
 
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email });
-    if (!user) return res.status(401).send("User do not exist");
-    if (user.password !== password) return res.status(401).send("Wrong Password");
-    const payload = {
-        check: true,
-        userId: user._id
-    };
-    const token = jwt.sign(payload, process.env.SEED, {
-        expiresIn: 1440
-    });
+//     const { email, password } = req.body;
+//     const user = await User.findOne({ email: email });
+//     if (!user) return res.status(401).send("User do not exist");
+//     if (user.password !== password) return res.status(401).send("Wrong Password");
+//     const payload = {
+//         check: true,
+//         userId: user._id
+//     };
+//     const token = jwt.sign(payload, process.env.SEED, {
+//         expiresIn: 1440
+//     });
 
-    res.json({
-        mensaje: 'Autenticación correcta',
-        token: token,
-        _id: payload.userId
-    });
-})
+//     res.json({
+//         mensaje: 'Autenticación correcta',
+//         token: token,
+//         _id: payload.userId
+//     });
+// })
 
-module.exports = app;
+// module.exports = app;
